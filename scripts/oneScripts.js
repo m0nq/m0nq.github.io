55ad75a684f555c93e6ae17a38f9e70cab309686
Monk Wellington <Vishnu@Vishnu.local>
2016-08-23 14:14:43 -0700

    fix the scroll timing to be a little slower.

--- scripts/oneScripts.js
+++ scripts/oneScripts.js
@@ -1,35 +1,37 @@
-$(window).load(function swapper() {
-  if (window.innerWidth < 480) {
-    $('.navbar-brand').text("MW");
-  } else {
-    $('.navbar-brand').text("Monk Wellington");
-  }
-});
+$(function () {
+  $(window).load(function swapper() {
+    if (window.innerWidth < 480) {
+      $('.navbar-brand').text("MW");
+    } else {
+      $('.navbar-brand').text("Monk Wellington");
+    }
+  });
 
-$(window).resize(function () {
-  if (window.innerWidth < 480) {
-    $('.navbar-brand').text("MW");
-  } else {
-    $('.navbar-brand').text("Monk Wellington");
-  }
-});
+  $(window).resize(function () {
+    if (window.innerWidth < 480) {
+      $('.navbar-brand').text("MW");
+    } else {
+      $('.navbar-brand').text("Monk Wellington");
+    }
+  });
 
-$('a').click(function (e) {
-  e.preventDefault();
-  var $section = $(this).context.innerText;
-  switch ($section) {
-    case 'Web':
-    case 'Video':
-    case 'Audio':
-    case 'About':
-      $('html, body').animate({
-        scrollTop : $('#' + $section.toLowerCase()).offset().top - 80
-      });
-      break;
-    case 'Monk Wellington':
-      $('html, body').animate({
-        scrollTop : $('#top').offset().top - 80
-      });
-      break;
-  }
+  $('a').click(function (e) {
+    e.preventDefault();
+    var $section = $(this).context.innerText;
+    switch ($section) {
+      case 'Web':
+      case 'Video':
+      case 'Audio':
+      case 'About':
+        $('html, body').animate({
+          scrollTop : $('#' + $section.toLowerCase()).offset().top - 80
+        }, 600);
+        break;
+      case 'Monk Wellington':
+        $('html, body').animate({
+          scrollTop : $('#top').offset().top - 80
+        }, 600);
+        break;
+    }
+  });
 });
