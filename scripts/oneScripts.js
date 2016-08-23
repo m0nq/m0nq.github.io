1931fdefa53681f47114737fc4e4475f9dfad917
Monk Wellington <Vishnu@Vishnu.local>
2016-08-23 13:52:51 -0700

    update html for dropdown. refactor js click event.

--- scripts/oneScripts.js
+++ scripts/oneScripts.js
@@ -14,26 +14,22 @@ $(window).resize(function () {
   }
 });
 
-$('a')[0].addEventListener('click', function (e) {
+$('a').click(function (e) {
   e.preventDefault();
-  var $top = $('#top').offset().top
-  $('html, body').animate({
-    scrollTop: $top
-  }, 800);
-});
-
-$('a')[1].addEventListener('click', function (e) {
-  e.preventDefault();
-  var $about = $('#about').offset().top - 80
-  $('html, body').animate({
-    scrollTop: $about
-  }, 800);
-});
-
-$('a')[2].addEventListener('click', function (e) {
-  e.preventDefault();
-  var $projects = $('#projects').offset().top - 80
-  $('html, body').animate({
-    scrollTop: $projects
-  }, 800);
+  var $section = $(this).context.innerText;
+  switch ($section) {
+    case 'Web':
+    case 'Video':
+    case 'Audio':
+    case 'About':
+      $('html, body').animate({
+        scrollTop : $('#' + $section.toLowerCase()).offset().top - 80
+      });
+      break;
+    case 'Monk Wellington':
+      $('html, body').animate({
+        scrollTop : $('#top').offset().top - 80
+      });
+      break;
+  }
 });
